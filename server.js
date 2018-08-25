7bff084e1840e1c0f96459a0a944d2a19adf1609
Scott Rock <scottrockbiz@gmail.com>
2018-08-24 20:01:15 -0700

    adding and displaying quotes, no error handling

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
+const quoteSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+    minlength: [2, 'Name should be at least 2 characters long'],
+  },
+  quote: {
+    type: String,
+    required: [true, 'Quote is required'],
+    trim: true,
+  },
+}, { timestamps: true });
+const Quote = mongoose.model('Quote', quoteSchema);
+
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('views'));
 app.use(express.static(path.resolve('static')));
@@ -19,6 +34,19 @@ app.use(parser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
   response.render('index');
+});
+
+app.get('/quotes', function(request, response) {
+  Quote.find({})
+    .then(quotes => response.render('quotes/index', { quotes }))
+    .catch(console.log);
+});
+
+app.post('/quotes', function(request, response) {
+  console.log(request.body);
+  Quote.create(request.body)
+    .then(response.redirect('/quotes'))
+    .catch(console.log);
 });
 
 app.listen(port, console.log(`quoting app listening on port ${port}`));
